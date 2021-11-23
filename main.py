a6abdc2d7f888aa9faf7cb775494cc5815c2be6a
Mohammed Alessi <ma.al_essi@icloud.com>
2021-11-23 19:52:53 +0300

    refactoring

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Collection
 from requests.api import get
 from selenium import webdriver
 import selenium
@@ -5,27 +6,35 @@ import json
 import time
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import urllib.parse
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
 import requests
 from terminalColors import *
 import os
 
 
-HTML_PARSER = "html.parser"
+HTML_PARSER = "lxml"
 BASE_URL = "https://www.faselhd.pro/"
+PLAYER = "iina"
+	
+	
+def input_is_valid(n, mn, mx) :
+	if n.isdigit():
+		if n < mn or n > mx:
+			return False
+	else :
+		return False
+	
+	return True
 
-CLI_COMMAND = "unbuffer -p /usr/local/bin/iina-cli '<M3U8 Link>'"
+	
 
 caps = DesiredCapabilities.CHROME
 caps['goog:loggingPrefs'] = {'performance': 'ALL'}
 
 options = webdriver.ChromeOptions()
-# options.add_argument('--no-sandbox')
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-# /Applications/Google Chrome.app/Contents/MacOS/Google Chrome
-
 driver = webdriver.Chrome("bin/chromedriver", options=options, desired_capabilities=caps)
 
 
@@ -94,7 +103,10 @@ def get_anime_episodes(search_result) :
 	links = episodes_div.find_all("a")
 	
 	for link in links:
-		episodes[link.text.strip()] = link["href"]
+		episode = {}
+		episode["link"] = link["href"]
+		episode["title"] = link.text.strip()
+		episodes.append(episode)
 	
 	return episodes
 
@@ -113,24 +125,15 @@ def get_m3u8_link(episode_link):
 			frame_link = frame["src"]
 			break
 	
-	# print("frame_link = ", frame_link)
 	driver.get(frame_link)
 	frame_soup = BeautifulSoup(driver.page_source, HTML_PARSER)
 	
-	# print(frame_soup.prettify())
 	
 	buttons_div = frame_soup.find("div", class_="quality_change")
 	quilities_buttons = buttons_div.find_all("button", class_="hd_btn")
-	# print("quilities_buttons = ", quilities_buttons)
 	quilities = [int(btn["data-quality"]) if btn["data-quality"] != "auto" else 0 for btn in quilities_buttons]
-	# print("quilities = ", quilities)
 	highest_quality = max(quilities)
 	
-	# try :
-	# 	button.click()
-	# except selenium.common.exceptions.ElementClickInterceptedException :
-	# 	print("ElementClickInterceptedException: clicking again after 5 sec")
-	# 	button.click()
 	
 	driver.execute_script("$('.hd_btn').click();")
 	time.sleep(0.5)
@@ -140,14 +143,11 @@ def get_m3u8_link(episode_link):
 		events = [process_browser_log_entry(entry) for entry in browser_log]
 		events = [event for event in events if 'Network.response' in event['method']]
 		for event in events:
-			# print("evern  = ", json.dumps(event))
 			try : 
 				url = event['params']['response']['url']
 			except KeyError :
 				continue
 			if "faselhdstream.com/stream/hls" in url :
-				# driver.quit()
-				# print("="*100, "fount the url", "\n", url)
 				return event['params']['response']['url']
 	
 	
@@ -161,71 +161,96 @@ def contains_seasons(search_result):
 	return seasons_div != None
 	
 
+
+def show_table(elements, color1, color2) :
+	for index, element in enumerate(elements):
+		if index % 2 == 0 :
+			color_print(f"[{index + 1}] {element}", color1)
+		else :
+			color_print(f"[{index + 1} {element}", color2)
+	
+
+
+def controller(episodes, currentIndex) :
+	
+	present_player_with_episode(episodes[currentIndex - 1])
+	color_print(f"Currently playing [{currentIndex} / {len(episodes)}]", tcolors.OKGREEN)
+
+	if currentIndex < len(episodes) :
+		color_print(f"	[n] to play next episode", tcolors.OKCYAN)
+	
+	if currentIndex > 1 :
+		color_print(f"	[p] to play previous episode", tcolors.OKBLUE)
+	
+	
+	color_print(f"	[q] to exit", tcolors.WARNING)
+	
+	choice = color_input(f"	[*] choice: ", tcolors.OKGREEN)
+	
+	if choice.lower() == "n" :
+		controller(episodes, currentIndex + 1)
+	elif choice.lower() == "p" :
+		controller(episodes, currentIndex - 1)
+	elif choice.lower() == "q" :
+		return
+	else :
+		color_print("Invalid input", tcolors.FAIL)
+		return
+
+
+def present_player_with_episode(episode) :
+	m3u8_link = get_m3u8_link(episode["link"])
+	
+	if PLAYER == "iina" :
+		cli_command = "unbuffer -p /usr/local/bin/iina-cli " + m3u8_link
+		os.system(cli_command)
+	
+
+
 def main() :
 	search_query = color_input("[ * ] - Enter search query: ", tcolors.OKGREEN)
 	results = search(search_query)
 	
-	for index, anime in enumerate(results):
-		color_print("[ " + str(index + 1) + " ] : " + anime["title"], tcolors.OKCYAN)
+	show_table([result["title"] for result in results], tcolors.OKCYAN, tcolors.OKBLUE)
 	
-	result_number = color_input("[ * ] - Enter number: ", tcolors.OKGREEN)
+	enterd_choice = color_input("[ * ] - Enter number: ", tcolors.OKGREEN)
 	
-	if result_number.isdigit():
-		result_number = int(result_number) - 1
-		if result_number < 0 or result_number >= len(results):
-			color_print("[ * ] - Invalid number", tcolors.FAIL)
-			return
-	else :
-		color_print("[ ERROR ] - Invalid number", tcolors.FAIL)
+	if not input_is_valid(enterd_choice, 1, len(results)) :
+		color_print(f"[ ERROR] invalid input")
 		return
 	
-	anime = results[result_number]
+	choosen_result = results[enterd_choice - 1]
 	
-	if contains_seasons(anime) :
-		seasons = get_seasons(anime)
-		for index, season in enumerate(seasons):
-			color_print("[ " + str(index + 1) + " ] : " + season["title"], tcolors.OKCYAN)
+	if contains_seasons(choosen_result) :
+		seasons = get_seasons(choosen_result)
+		show_table([season["title"] for season in seasons], tcolors.OKCYAN, tcolors.OKBLUE)
 		
-		season = color_input("[ * ] - Enter number: ", tcolors.OKGREEN)
-		if season.isdigit():
-			season = int(season) - 1
-			if season < 0 or season >= len(seasons):
-				color_print("[ * ] - Invalid number", tcolors.FAIL)
-				return
-		else :
-			color_print("[ ERROR ] - Invalid number", tcolors.FAIL)
+		enterd_choice = color_input("[ * ] - Enter number: ", tcolors.OKGREEN)
+		if not input_is_valid(enterd_choice, 1, len(seasons)) :
+			color_print(f"[ ERROR] invalid input", tcolors.FAIL)
 			return
 		
-		season = seasons[season]
+		season = seasons[enterd_choice - 1]
 		episodes = get_anime_episodes(season)
 	else :
-		episodes = get_anime_episodes(anime)
+		episodes = get_anime_episodes(choosen_result)
 	
-	for index, (episode, link) in enumerate(episodes.items()):
-		color_print(f"[ {index + 1} ] - {episode} ", tcolors.OKCYAN)
+	
+	show_table([episode["title"] for episode in episodes])
 	
 	episode_number = color_input("[ * ] - Enter episode number: ", tcolors.OKGREEN)
+	enterd_choice = color_input("[ * ] - Enter number: ", tcolors.OKGREEN)
 	
-	if episode_number.isdigit():
-		episode_number = int(episode_number) - 1
-		if episode_number < 0 or episode_number >= len(episodes):
-			color_print("[ * ] - Invalid number", tcolors.FAIL)
-			return
-	else :
-		color_print("[ ERROR ] - Invalid number", tcolors.FAIL)
+	if not input_is_valid(enterd_choice, 1, len(episodes)) :
+		color_print(f"[ ERROR] invalid input", tcolors.FAIL)
 		return
 	
-	selected_episode = episodes[list(episodes.keys())[episode_number]]
-	m3u8_link = get_m3u8_link(selected_episode)
 	
-	command = CLI_COMMAND.replace("<M3U8 Link>", m3u8_link)
-	os.system(command)
+	controller(episodes, enterd_choice)
 	
 	
 	
 	
 	
-	
-	
-	
+
 main()
