a9a653000c4b802598e0286c5d0bdc07acc0cf00
Mohammed Alessi <ma.al_essi@icloud.com>
2024-12-10 10:27:06 +0300

    add: ansi colors

--- terminal_colors.py
+++ terminal_colors.py
@@ -1,16 +1,17 @@
 class TerminalColors:
-    '''
+    """
     terminal colors codes
-    '''
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    """
+
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
 
 
 def color_print(text, color):
